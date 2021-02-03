d02e1c86100dd624cd4a7a85d50ac08e9876941c
Cédric Aellen <cedric.aellen@vd.ch>
2021-02-03 06:47:55 +0100

    beautify code

--- fractal.config.js
+++ fractal.config.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
 /* Create a new Fractal instance and export it for use elsewhere if required */
-const fractal = (module.exports = require("@frctl/fractal").create());
+const fractal = (module.exports = require('@frctl/fractal').create());
 
 /* Set the title of the project */
-fractal.set("project.title", "Jorant design system");
+fractal.set('project.title', 'Jorant design system');
 
 /* Tell Fractal where the components will live */
-fractal.components.set("path", __dirname + "/src/components");
-fractal.components.engine("@frctl/nunjucks"); // Register the Nunjucks adapter for your components
-fractal.components.set("ext", ".nunj"); // Look for files with a .nunj file extension
-fractal.components.set("default.status", "wip");
+fractal.components.set('path', __dirname + '/src/components');
+fractal.components.engine('@frctl/nunjucks'); // Register the Nunjucks adapter for your components
+fractal.components.set('ext', '.nunj'); // Look for files with a .nunj file extension
+fractal.components.set('default.status', 'wip');
 
 /* Tell Fractal where the documentation pages will live */
-fractal.docs.set("path", __dirname + "/src/docs");
+fractal.docs.set('path', __dirname + '/src/docs');
 
-fractal.web.set("static.path", __dirname + "/public");
-fractal.web.set("builder.dest", __dirname + "/build");
+fractal.web.set('static.path', __dirname + '/public');
+fractal.web.set('builder.dest', __dirname + '/build');
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
-    "postcss-easy-import": {},
+    'postcss-easy-import': {},
     tailwindcss: {},
-    autoprefixer: {},
-  },
+    autoprefixer: {}
+  }
 };
