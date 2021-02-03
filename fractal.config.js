df612980dcd0b30015d53f87822e884617bd91dd
Cédric Aellen <cedric.aellen@vd.ch>
2021-02-03 06:58:49 +0100

    add time of build in the sidebar informations

--- fractal.config.js
+++ fractal.config.js
@@ -2,6 +2,28 @@
 
 /* Create a new Fractal instance and export it for use elsewhere if required */
 const fractal = (module.exports = require('@frctl/fractal').create());
+//
+// require the Mandelbrot theme module
+const mandelbrot = require('@frctl/mandelbrot');
+
+// Create a new instance with custom config options
+const myCustomisedTheme = mandelbrot({
+  information: [
+    {
+      label: 'Version',
+      value: require('./package.json').version
+    },
+    {
+      label: 'Built on',
+      value: new Date(),
+      type: 'time', // Outputs a <time /> HTML tag
+      format: value => {
+        return value.toLocaleString('CH-fr');
+      }
+    }
+  ]
+  // Any other theme configuration values here
+});
 
 /* Set the title of the project */
 fractal.set('project.title', 'Jorant design system');
@@ -17,3 +39,6 @@ fractal.docs.set('path', __dirname + '/src/docs');
 
 fractal.web.set('static.path', __dirname + '/public');
 fractal.web.set('builder.dest', __dirname + '/build');
+
+// Tell Fractal to use the configured theme by default
+fractal.web.theme(myCustomisedTheme);
