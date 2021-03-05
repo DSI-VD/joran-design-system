535ef5cd01ca3b07b57d12f6a9833ab8b7c5dbb6
Cédric Aellen <cedric.aellen@vd.ch>
2021-03-05 14:21:12 +0100

    beautify

--- src/components/04-Components/testimonial/testimonial.js
+++ src/components/04-Components/testimonial/testimonial.js
@@ -1,42 +1,39 @@
-// cut the mustard
-if ('querySelector' in document && 
-    'localStorage' in window && 
+// Cut the mustard
+if ('querySelector' in document &&
+    'localStorage' in window &&
     'addEventListener' in window) {
+  const toggleButtons = document.querySelectorAll('.js-testimonial__toggle-btn');
+  const fullTextWrappers = document.querySelectorAll('.js-testimonial__fulltext');
+  let fullText;
+  let toggleButtonText;
 
-    var toggleButtons = document.querySelectorAll('.js-testimonial__toggle-btn');
-    var fullTextWrappers = document.querySelectorAll('.js-testimonial__fulltext');
-    var fullText;
-    var toggleButtonText;
-    
+  [].forEach.call(fullTextWrappers, fullTextWrapper => {
+    // Hide all full text on load
+    fullTextWrapper.setAttribute('hidden', true);
+  });
 
-    [].forEach.call(fullTextWrappers, function(fullTextWrapper) {
-        // hide all full text on load
+  [].forEach.call(toggleButtons, toggleButton => {
+    // Show toggle more buttons
+    toggleButton.removeAttribute('hidden');
+
+    // Add listener for each button
+    toggleButton.addEventListener('click', function () {
+      fullTextWrapper = this.parentElement.querySelector('.js-testimonial__fulltext');
+      toggleButtonText = this.querySelector('.js-testimonial__toggle-btn-text');
+
+      // Change attributes and text if full text is shown/hidden
+      console.log(fullTextWrapper.hasAttribute('hidden'));
+      if (!fullTextWrapper.hasAttribute('hidden')) {
+        toggleButtonText.textContent = 'Lire la suite';
         fullTextWrapper.setAttribute('hidden', true);
+        toggleButton.setAttribute('aria-expanded', false);
+        toggleButton.setAttribute('aria-pressed', false);
+      } else {
+        toggleButtonText.textContent = 'Reduire';
+        fullTextWrapper.removeAttribute('hidden');
+        toggleButton.setAttribute('aria-expanded', true);
+        toggleButton.setAttribute('aria-pressed', true);
+      }
     });
-
-    [].forEach.call(toggleButtons, function(toggleButton) {
-        // show toggle more buttons
-        toggleButton.removeAttribute('hidden');
-
-        // add listener for each button
-        toggleButton.addEventListener('click', function () {
-
-            fullTextWrapper = this.parentElement.querySelector('.js-testimonial__fulltext');
-            toggleButtonText = this.querySelector('.js-testimonial__toggle-btn-text');
-
-            // change attributes and text if full text is shown/hidden
-            console.log(fullTextWrapper.hasAttribute('hidden'));
-            if (!fullTextWrapper.hasAttribute('hidden')) {
-                toggleButtonText.innerText = 'Lire la suite';
-                fullTextWrapper.setAttribute('hidden', true);
-                toggleButton.setAttribute('aria-expanded', false);
-                toggleButton.setAttribute('aria-pressed', false);
-            } else {
-                toggleButtonText.innerText = 'Reduire';
-                fullTextWrapper.removeAttribute('hidden');
-                toggleButton.setAttribute('aria-expanded', true);
-                toggleButton.setAttribute('aria-pressed', true);
-            }
-        });
-    });
+  });
 }
