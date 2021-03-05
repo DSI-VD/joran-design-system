faf6b98fddc84e4eebc8fc47acb0fcebd5844522
Cédric Aellen <cedric.aellen@vd.ch>
2021-03-05 14:37:30 +0100

    Updates

---
+++ assets/scripts/testimonial.js
@@ -0,0 +1,39 @@
+// Cut the mustard
+if ('querySelector' in document &&
+    'localStorage' in window &&
+    'addEventListener' in window) {
+  const toggleButtons = document.querySelectorAll('.js-testimonial__toggle-btn');
+  const fullTextWrappers = document.querySelectorAll('.js-testimonial__fulltext');
+  let fullText;
+  let toggleButtonText;
+
+  [].forEach.call(fullTextWrappers, fullTextWrapper => {
+    // Hide all full text on load
+    fullTextWrapper.setAttribute('hidden', true);
+  });
+
+  [].forEach.call(toggleButtons, toggleButton => {
+    // Show toggle more buttons
+    toggleButton.removeAttribute('hidden');
+
+    // Add listener for each button
+    toggleButton.addEventListener('click', function () {
+      fullTextWrapper = this.parentElement.querySelector('.js-testimonial__fulltext');
+      toggleButtonText = this.querySelector('.js-testimonial__toggle-btn-text');
+
+      // Change attributes and text if full text is shown/hidden
+      console.log(fullTextWrapper.hasAttribute('hidden'));
+      if (!fullTextWrapper.hasAttribute('hidden')) {
+        toggleButtonText.textContent = 'Lire la suite';
+        fullTextWrapper.setAttribute('hidden', true);
+        toggleButton.setAttribute('aria-expanded', false);
+        toggleButton.setAttribute('aria-pressed', false);
+      } else {
+        toggleButtonText.textContent = 'Reduire';
+        fullTextWrapper.removeAttribute('hidden');
+        toggleButton.setAttribute('aria-expanded', true);
+        toggleButton.setAttribute('aria-pressed', true);
+      }
+    });
+  });
+}
