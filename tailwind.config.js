be85bd1d22ab9854bbaaead599a98e92b81579bc
Cédric Aellen <cedric.aellen@vd.ch>
2021-03-09 15:32:48 +0100

    install tailwindcss/aspect-ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -137,6 +137,7 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp')
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
   ]
 };
