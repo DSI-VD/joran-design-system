c6cadf97eda2c43a634a0cbdd913393b8c864715
Cédric Aellen <cedric.aellen@vd.ch>
2021-03-17 10:36:26 +0000

    reformat date

--- src/components/04-Components/slider/slider.config.js
+++ src/components/04-Components/slider/slider.config.js
@@ -3,12 +3,13 @@
 const faker = require('faker'); // require the faker module
 const articleCount = 20; // how many articles we should generate data for
 const articleData = [];
+const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
 
 for (var i = 0; i < articleCount; i++) {
     articleData.push({
         title: faker.lorem.sentence(),
         url: faker.internet.url(),
-        date: faker.date.past(),
+        date: faker.date.past().toLocaleDateString('fr-CH', dateOptions),
         image: {
             src: faker.image.unsplash.image('500','500', faker.random.word()),
             alt: faker.lorem.words(),
