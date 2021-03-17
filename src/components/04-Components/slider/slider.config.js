69955695d9452c45395225deda22ad61c91bfb28
Cédric Aellen <cedric.aellen@vd.ch>
2021-03-17 09:11:07 +0000

    use faker to generate data

---
+++ src/components/04-Components/slider/slider.config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const faker = require('faker'); // require the faker module
+const articleCount = 20; // how many articles we should generate data for
+const articleData = [];
+
+for (var i = 0; i < articleCount; i++) {
+    articleData.push({
+        title: faker.lorem.words(),
+        url: faker.internet.url(),
+        date: faker.date.past(),
+        image: {
+            src: faker.image.unsplash.image('500','500', faker.random.word()),
+            alt: faker.lorem.words(),
+        }
+    });
+}
+
+module.exports = {
+    context: {
+        title: "Slider",
+        articles: articleData // use our generated list of articles as context data for our template.
+    }
+};
