b1e68d69888846459e1b8e90dea774c3c1d9b1c5
Jérémie Roulin <jeremie.roulin@ideative.ch>
2021-03-19 14:00:14 +0100

    Re-organize javascript

--- src/components/04-Components/navigation/navigation.js
+++ src/components/04-Components/navigation/navigation.js
@@ -1,13 +1,52 @@
-init = () => {
-  const levelIndicator = document.querySelector('.c-nav__level-indicator');
-  let lastScrollPos = 0;
-  let ticking = false;
-  const nav = document.querySelector('.c-nav');
-  let navOpen = false;
-  let currentLevel = 0;
-  const navToggle = document.querySelector('.c-nav__toggle');
+class MobileNavigation {
+  constructor(nav) {
+    this.levelIndicator = document.querySelector('.c-nav__level-indicator');
+    this.nav = nav;
+    this.navOpen = false;
+    this.currentLevel = 0;
+    this.navToggle = document.querySelector('.c-nav__toggle');
 
-  const closeAllTabs = event => {
+    // Open menu tab
+    for (const item of document.querySelectorAll('.c-nav__list-link--w-children')) {
+      item.addEventListener('click', event => {
+        if (this.currentLevel === Number.parseInt(event.target.parentNode.dataset.level)) {
+          this.levelIndicator.append(this.createLevelIndicator(event.target));
+          event.target.closest('.c-nav__list').classList.add('js-c-nav__list--hide');
+          event.target.parentNode.querySelector('.c-nav__list').classList.add('js-c-nav__list--open');
+          item.setAttribute('aria-expanded', true);
+          this.currentLevel++;
+        } else if (event.target.parentNode.parentNode.matches('.c-nav__list--level-1')) {
+          this.closeLevelTabs(event, 2);
+        } else {
+          this.closeAllTabs(event);
+        }
+      });
+    }
+
+    // Open navigation
+    this.navToggle.addEventListener('click', (event) => this.toggle(event));
+  }
+
+
+
+  toggle(event) {
+    if (!this.navOpen) {
+      document.body.classList.add('js-b-nav--open');
+      this.navToggle.querySelector('.c-menu-icon').classList.add('js-c-menu-icon--open');
+      this.nav.classList.add('js-c-nav--open');
+      this.navOpen = true;
+    } else {
+      document.body.classList.remove('js-b-nav--open');
+      this.navToggle.querySelector('.c-menu-icon').classList.remove('js-c-menu-icon--open');
+      this.nav.classList.remove('js-c-nav--open');
+      this.closeAllTabs(event);
+      this.navOpen = false;
+    }
+
+    this.navToggle.setAttribute('aria-expanded', this.navOpen);
+  }
+
+  closeAllTabs(event) {
     for (const item of document.querySelectorAll('.c-nav__list')) {
       item.classList.remove('js-c-nav__list--open');
       item.classList.remove('js-c-nav__list--hide');
@@ -15,15 +54,15 @@ init = () => {
         button.setAttribute('aria-expanded', false);
       }
 
-      for (const item of levelIndicator.querySelectorAll('li')) {
+      for (const item of this.levelIndicator.querySelectorAll('li')) {
         item.remove();
       }
     }
 
-    currentLevel = 0;
-  };
+    this.currentLevel = 0;
+  }
 
-  const closeLevelTabs = (event, level) => {
+  closeLevelTabs(event, level) {
     for (const item of document.querySelectorAll('.c-nav__list--level-' + (level - 1))) {
       item.classList.remove('js-c-nav__list--hide');
     }
@@ -33,23 +72,23 @@ init = () => {
       item.parentNode.querySelector('.c-nav__list-link--w-children').setAttribute('aria-expanded', false);
     }
 
-    const levelElement = levelIndicator.querySelector('li:nth-child(' + level + ')');
+    const levelElement = this.levelIndicator.querySelector('li:nth-child(' + level + ')');
     levelElement.remove();
-    currentLevel = 1;
-  };
+    this.currentLevel = 1;
+  }
 
-  const createLevelIndicator = target => {
+  createLevelIndicator(target) {
     const levelLink = document.createElement('button');
     levelLink.setAttribute('tabindex', '-1');
     const levelText = document.createTextNode(target.innerHTML);
     levelLink.append(levelText);
     if (target.parentNode.parentNode.matches('.c-nav__list--level-1')) {
       levelLink.addEventListener('click', event => {
-        closeLevelTabs(event, 2);
+        this.closeLevelTabs(event, 2);
       });
     } else {
       levelLink.addEventListener('click', event => {
-        closeAllTabs(event);
+        this.closeAllTabs(event);
       });
     }
 
@@ -57,57 +96,50 @@ init = () => {
     level.append(levelLink);
     return level;
   };
+}
 
-  // Open menu tab
-  for (const item of document.querySelectorAll('.c-nav__list-link--w-children')) {
-    item.addEventListener('click', event => {
-      if (currentLevel === Number.parseInt(event.target.parentNode.dataset.level)) {
-        levelIndicator.append(createLevelIndicator(event.target));
-        event.target.closest('.c-nav__list').classList.add('js-c-nav__list--hide');
-        event.target.parentNode.querySelector('.c-nav__list').classList.add('js-c-nav__list--open');
-        item.setAttribute('aria-expanded', true);
-        currentLevel++;
-      } else if (event.target.parentNode.parentNode.matches('.c-nav__list--level-1')) {
-        closeLevelTabs(event, 2);
-      } else {
-        closeAllTabs(event);
-      }
-    });
+class DesktopNavigation {
+  constructor(nav) {
+    this.nav = nav;
+  }
+}
+
+class StickyNavigation {
+  constructor(nav) {
+    this.lastScrollPos = 0;
+    this.ticking = false;
+    this.nav = nav;
+    window.addEventListener('scroll', (event) => this.sticky(event));
   }
 
-  // Open navigation
-  navToggle.addEventListener('click', event => {
-    if (!navOpen) {
-      document.body.classList.add('js-b-nav--open');
-      navToggle.querySelector('.c-menu-icon').classList.add('js-c-menu-icon--open');
-      nav.classList.add('js-c-nav--open');
-      navOpen = true;
-    } else {
-      document.body.classList.remove('js-b-nav--open');
-      navToggle.querySelector('.c-menu-icon').classList.remove('js-c-menu-icon--open');
-      nav.classList.remove('js-c-nav--open');
-      closeAllTabs(event);
-      navOpen = false;
-    }
-
-    navToggle.setAttribute('aria-expanded', navOpen);
-  });
-
-  // Sticky nav
-  window.addEventListener('scroll', event => {
-    lastScrollPos = window.scrollY;
-    if (!ticking) {
+  sticky(event) {
+    this.lastScrollPos = window.scrollY;
+    if (!this.ticking) {
       window.requestAnimationFrame(() => {
-        if (lastScrollPos > 50) {
-          nav.classList.add('js-c-nav--sticky');
+        if (this.lastScrollPos > 50) {
+          this.nav.classList.add('js-c-nav--sticky');
         } else {
-          nav.classList.remove('js-c-nav--sticky');
+          this.nav.classList.remove('js-c-nav--sticky');
         }
 
-        ticking = false;
+        this.ticking = false;
       });
     }
 
-    ticking = true;
-  });
+    this.ticking = true;
+  }
+}
+
+const initNavigation = () => {
+  new StickyNavigation(document.querySelector('.c-nav'));
+  const init = event => {
+    if (window.innerWidth < 1280) {
+      new MobileNavigation(document.querySelector('.c-nav'));
+    } else {
+      new DesktopNavigation(document.querySelector('.c-nav'));
+    }
+  };
+  init();
 };
+
+document.addEventListener('DOMContentLoaded', initNavigation);
