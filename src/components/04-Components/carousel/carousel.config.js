1ad7e30ee714147d5d8001a3ca4032cee1684313
Cédric Aellen <cedric.aellen@vd.ch>
2021-03-24 15:32:31 +0100

    refactor

--- src/components/04-Components/carousel/carousel.config.js
+++ src/components/04-Components/carousel/carousel.config.js
@@ -6,34 +6,13 @@ const articleData = [];
 
 for (var i = 0; i < articleCount; i++) {
   let randomWord = faker.random.word();
+
   articleData.push({
     image: {
       styleModifier: 'c-carousel__figure',
       src: faker.image.unsplash.image('1024','576', randomWord),
       alt: faker.lorem.words(),
       caption: faker.lorem.sentence(),
-      srcset: [
-        {
-          url: faker.image.unsplash.image('320','180', randomWord),
-          width: '320w'
-        },
-        {
-          url: faker.image.unsplash.image('540','303', randomWord),
-          width: '540w'
-        },
-        {
-          url: faker.image.unsplash.image('720','405', randomWord),
-          width: '720w'
-        },
-        {
-          url: faker.image.unsplash.image('960','540', randomWord),
-          width: '960w'
-        },
-        {
-          url: faker.image.unsplash.image('1140','641', randomWord),
-          width: '1140w'
-        }
-      ]
     }
   });
 }
