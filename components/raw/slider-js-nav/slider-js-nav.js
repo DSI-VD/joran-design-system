7439f4337ae45081f0ff5173c1225ed7fcb04ae0
Cédric Aellen <cedric.aellen@vd.ch>
2021-05-21 10:08:36 +0200

    Updates

--- components/raw/slider-js-nav/slider-js-nav.js
+++ components/raw/slider-js-nav/slider-js-nav.js
@@ -1,24 +1,25 @@
 const initSlider = () => {
   for (const item of document.querySelectorAll('.c-slider-js-nav-carousel')) {
-    let sidebar = item.parentNode.parentNode.querySelector('.c-slider-js-nav__container-left');
+    const sidebar = item.parentNode.parentNode.querySelector('.c-slider-js-nav__container-left');
     let highest = 0;
     new Flickity(item, {
       contain: true,
       imagesLoaded: true,
       on: {
-        ready: function () {
+        ready() {
           for (const card of item.querySelectorAll('.c-slider-js-nav__article')) {
             if (card.offsetHeight > highest) {
               highest = card.offsetHeight;
             }
           }
-          sidebar.querySelector('.c-slider-js-nav__sidebar-list').style.height = (highest + 50) + "px";
+
+          sidebar.querySelector('.c-slider-js-nav__sidebar-list').style.height = (highest + 50) + 'px';
         },
-        change: function (index) {
+        change(index) {
           if (index > 0) {
-            sidebar.classList.add('c-slider-js-nav__container-left--hidden')
+            sidebar.classList.add('c-slider-js-nav__container-left--hidden');
           } else {
-            sidebar.classList.remove('c-slider-js-nav__container-left--hidden')
+            sidebar.classList.remove('c-slider-js-nav__container-left--hidden');
           }
         }
       }
